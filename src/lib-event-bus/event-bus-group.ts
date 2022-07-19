bf0aa03acaa5a8373dece7cf29ab932863f807f7
Benjamin Jesuiter <bjesuiter@gmail.com>
2022-07-19 23:42:21 +0200

    add this.on<T> method on event-bus-group

--- src/lib-event-bus/event-bus-group.ts
+++ src/lib-event-bus/event-bus-group.ts
@@ -1,5 +1,9 @@
 import { Subscription } from "rxjs";
 import { EventBus } from "./event-bus";
+import { EventBusCallback } from "./types/event-bus-callback";
+import { EventBusErrorCallback } from "./types/event-bus-error-callback";
+import { TypeDiscriminator } from "./types/type-discriminator";
+import { defaultErrorCallback } from "./utils/default-error-callback";
 
 /**
  * An EventBusGroup allows for using the event bus with a Callback-Interface.
@@ -11,7 +15,45 @@ import { EventBus } from "./event-bus";
 export class EventBusGroup {
   private subscriptions: Subscription[] = [];
 
-  constructor(private bus: EventBus) {
+  constructor(
+    private bus: EventBus,
+    private errorCallback: EventBusErrorCallback = defaultErrorCallback,
+  ) {
+  }
+
+  /**
+   * I subscribe to the message bus, but only invoke the callback when the event is
+   * of the given newable type (ie, it's a Class definition, not an instance).
+   * --
+   * NOTE: The NewableType<T> will allow for Type inference.
+   *
+   * @param typeFilter
+   * @param callback
+   * @param callbackContext
+   * @returns
+   */
+  public on<T>(
+    typeFilter: TypeDiscriminator<T>,
+    callback: EventBusCallback<T>,
+    callbackContext: any = null,
+    errorCallback?: EventBusErrorCallback,
+  ): void {
+    const sub = this.bus.on$(typeFilter).subscribe(
+      (event: T): void => {
+        try {
+          callback.call(callbackContext, event);
+        } catch (error) {
+          if (errorCallback) {
+            errorCallback.call(callbackContext, errorCallback);
+            return;
+          }
+
+          this.errorCallback.call(callbackContext, error);
+        }
+      },
+    );
+
+    this.subscriptions.push(sub);
   }
 
   public unsubscribeAll() {
