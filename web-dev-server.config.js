f2b119111f6866186a1516be1afccbe02ecb5c98
Benjamin Jesuiter <bjesuiter@gmail.com>
2022-07-19 21:53:31 +0200

    add web-dev-server config

---
+++ web-dev-server.config.js
@@ -0,0 +1,11 @@
+/**
+ * Config Docs: https://modern-web.dev/docs/dev-server/cli-and-configuration/#configuration-file
+ *
+ * package.json: type=module => this config will be loaded as esm
+ */
+
+export default {
+  open: true,
+  watch: true,
+  appIndex: 'index.html',
+};
